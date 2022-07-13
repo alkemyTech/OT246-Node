f98b4391d8f4ef199d88d49f981d24484f742d4e
ghnoob <ptnchk.rodrigo@gmail.com>
2022-07-13 15:53:47 -0300

    feat: Organization model

---
+++ models/organization.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Organization extends Model {}
+
+  Organization.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      address: DataTypes.STRING,
+      phone: DataTypes.INTEGER,
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      welcomeText: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      aboutUsText: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'Organization',
+      paranoid: true,
+    }
+  );
+
+  return Organization;
+};
