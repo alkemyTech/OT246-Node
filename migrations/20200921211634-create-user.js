73f4e259a00363250d03293041c021e0bf28e627
Brenda Álvarez <bgalacho@gmail.com>
2022-07-14 16:17:41 -0300

    User model and migration ready

--- migrations/20200921211634-create-user.js
+++ migrations/20200921211634-create-user.js
@@ -9,19 +9,25 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       firstName: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       lastName: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
       },
       password: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
-      image: {
-        type: Sequelize.STRING
+      photo: {
+        type: Sequelize.STRING,
+        allowNull: true
       },
       roleId: {
         type: Sequelize.INTEGER,
