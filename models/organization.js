b6de6290dccdfa58f2d114a7fb5d08d5b7fb63bf
ghnoob <ptnchk.rodrigo@gmail.com>
2022-07-13 22:06:18 -0300

    fix: wrong DataType

--- models/organization.js
+++ models/organization.js
@@ -25,7 +25,7 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.TEXT,
         allowNull: false,
       },
-      aboutUsText: DataTypes.STRING,
+      aboutUsText: DataTypes.TEXT,
     },
     {
       sequelize,
