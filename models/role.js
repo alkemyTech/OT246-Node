2d84b716f6c047de111da4bbfde77cf16aa907c2
SamuelOnti0023 <enrissmuelo@gmail.com>
2022-07-15 18:34:54 -0300

    feat: complete create Role model

--- models/role.js
+++ models/role.js
@@ -1,24 +1,25 @@
 'use strict';
-const {  Model} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Role extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  Role.init({
-    name: DataTypes.STRING,
-    description: DataTypes.STRING,
-    deletedAt: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'Role',
-    paranoid: true
-  });
-  return Role;
+    class Role extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    };
+    Role.init({
+
+        id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true, allowNull: false },
+        name: { type: DataTypes.STRING, allowNull: false },
+        description: DataTypes.STRING
+    }, {
+        sequelize,
+        modelName: 'Role',
+        paranoid: true,
+    });
+    return Role;
 };
