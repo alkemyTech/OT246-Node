ca2bba7a22e1219edf93b69b1f6ea140e1cf65f8
SamuelOnti0023 <enrissmuelo@gmail.com>
2022-07-15 20:23:03 -0300

    feat: Members model created

---
+++ models/members.js
@@ -0,0 +1,30 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class Member extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    };
+    Member.init({
+
+        id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true, allowNull: false },
+        name: { type: DataTypes.STRING, allowNull: false },
+        description: DataTypes.STRING,
+        facebookUrl: DataTypes.STRING,
+        instagramUrl: DataTypes.STRING,
+        linkedinUrl: DataTypes.STRING,
+        image: { type: DataTypes.STRING, allowNull: false },
+        description: DataTypes.STRING
+    }, {
+        sequelize,
+        modelName: 'Member',
+        paranoid: true
+    });
+    return Member;
+};
