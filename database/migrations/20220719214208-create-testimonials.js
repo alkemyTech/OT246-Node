be4797289079e6b08a7f706b9aea16be9ddc3d57
Maria Roberta Mendoza <trabajoescueladesarrollo@gmail.com>
2022-07-20 00:21:12 +0200

    Testimonials model and migrations

---
+++ database/migrations/20220719214208-create-testimonials.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const sequelize = require("sequelize");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('testimonials', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      content: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('testimonials');
+  }
+};
