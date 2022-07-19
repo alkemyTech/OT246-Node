7444bf61108a210c2fa5cc7f45f20ec410fc0e18
Gabriel Martinez Corti <79473217+gabodmc@users.noreply.github.com>
2022-07-19 18:56:18 -0300

    activity, role & user models and migrations added (#10)
    Co-authored-by: gabodmc <gcorti@infobae.com>

---
+++ database/migrations/20220719213849-create-user.js
@@ -0,0 +1,49 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      photo: {
+        type: Sequelize.STRING
+      },
+      roleId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Roles',
+          key: 'id'
+        }
+      },
+      deletedAt: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ database/models/role.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Role extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Role.hasMany(models.User, { foreignKey: 'roleId' });
+    }
+  };
+  Role.init({
+    name: DataTypes.STRING,
+    description: DataTypes.STRING,
+    deletedAt: {
+      type: Sequelize.STRING
+    },
+  }, {
+    sequelize,
+    timestamps: true,
+    modelName: 'Role',
+  });
+  return Role;
+};
