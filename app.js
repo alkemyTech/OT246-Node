3ad43ea3af320555c03e0a65d141a4dde4c40592
ghnoob <ptnchk.rodrigo@gmail.com>
2022-07-20 11:26:19 -0300

    move organization router import to router index file

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const cors = require('cors')
 require('dotenv').config()
 
 const indexRouter = require('./routes/index')
-const organizationsRouter = require('./routes/organizations')
 
 const port = process.env.PORT || 3001
 
@@ -25,7 +24,6 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-app.use('/organization', organizationsRouter)
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
