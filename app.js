140975f0344939a1a62a311dd20e21b3a2ae1b20
Martin Heredia <martinheredia.dev@gmail.com>
2022-07-20 12:12:28 -0300

    Created route slide, and import in app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 require('dotenv').config()
 
 const indexRouter = require('./routes/index')
+const slidesRouter = require('./routes/slides')
 
 const port = process.env.PORT || 3001
 
@@ -24,6 +25,8 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
+
+app.use('/slides', slidesRouter)
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
