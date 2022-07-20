24d7d03591c6d979791e75ea00232fad7b871f7f
Martin Heredia <martinheredia.dev@gmail.com>
2022-07-20 16:12:40 -0300

    fixed routes in index, and app.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const { get } = require('../controllers/index')
 const organizationsRouter = require('./organizations')
+const slidesRouter = require('./slides')
 
 const router = express.Router()
 
 // example of a route with index controller get function
 router.get('/', get)
 router.use('/organization', organizationsRouter)
+router.use('/slide', slidesRouter)
 
 module.exports = router
