786de60cbf9ef8ea3d5d3d67a6285dc91786bf98
ghnoob <ptnchk.rodrigo@gmail.com>
2022-07-21 16:49:37 -0300

    createUser method added

--- services/users.js
+++ services/users.js
@@ -1,6 +1,29 @@
 const { User } = require('../database/models')
+const { ErrorObject } = require('../helpers/error')
 
 exports.findOneByEmail = (email) => User.findOne({
   where: { email },
   attributes: { exclude: ['password'] },
 })
+
+exports.createUser = async ({
+  firstName, lastName, email, password,
+}) => {
+  try {
+    const existingUser = await this.findOneByEmail(email)
+
+    if (existingUser) {
+      throw new ErrorObject('Email is already in use', 400)
+    }
+
+    const newUser = await User.create({
+      firstName, lastName, email, password,
+    })
+
+    delete newUser.password
+
+    return newUser
+  } catch (error) {
+    throw new ErrorObject(error.message, error.statusCode || 500)
+  }
+}
