454182b4a5382b006bf589700ffba1c0c8df9c93
ghnoob <ptnchk.rodrigo@gmail.com>
2022-07-21 19:54:48 -0300

    validate request body

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,10 @@
 const { Router } = require('express')
 const { register } = require('../controllers/auth')
+const { validateSchema } = require('../middlewares/validations')
+const { registerBody } = require('../schemas/register')
 
 const router = Router()
 
-router.post('/register', register)
+router.post('/register', validateSchema(registerBody), register)
 
 module.exports = router
