36bb5ec1b4cda9669c949af33e7d8bbc3e06fe35
Rodrigo Pietnechuk <ptnchk.rodrigo@gmail.com>
2022-07-21 12:54:33 -0300

    [ENDPOINT] GET /organization/public: fix controller and service methods OT246-23 (#21)
    * fix service method signature. update jsdoc comment
    
    * fix method signature
    
    * call service from outside the return statement

--- controllers/organizations.js
+++ controllers/organizations.js
@@ -1,17 +1,19 @@
 const createHttpError = require('http-errors')
-const { get } = require('../services/organizations')
+const { getOrganization } = require('../services/organizations')
 const { catchAsync } = require('../helpers/catchAsync')
 const { endpointResponse } = require('../helpers/success')
 
 module.exports = {
   get: catchAsync(async (req, res, next) => {
     try {
+      const responseBody = await getOrganization()
+
       return endpointResponse({
         res,
         code: 200,
         status: true,
         message: 'OK',
-        body: await get(),
+        body: responseBody,
       })
     } catch (err) {
       const httpError = createHttpError(
