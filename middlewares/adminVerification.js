dd018967a09c91350249443798844ef0bed7bc7b
Tomas <tomasignaciocontreras@hotmail.com>
2022-07-21 14:04:26 -0300

    lint fix

--- middlewares/adminVerification.js
+++ middlewares/adminVerification.js
@@ -2,22 +2,21 @@ const { User } = require('../database/models/index')
 const { ErrorObject } = require('../helpers/error')
 
 exports.adminVerification = async (req, res, next) => {
-    try {
-        const { id } = req.params
-        const user = await User.findOne({
-            where: {
-                id
-            },
-            attributes: ["roleId"]
-        })
+  try {
+    const { id } = req.params
+    const user = await User.findOne({
+      where: {
+        id,
+      },
+      attributes: ['roleId'],
+    })
 
-        if (user && user.roleId === 1) {
-            next()
-        } else {
-            throw new ErrorObject("Unauthorized", 403)
-        }
-
-    } catch (error) {
-        throw new ErrorObject(error.message, error.statusCode || 500)
+    if (user && user.roleId === 1) {
+      next()
+    } else {
+      throw new ErrorObject('Unauthorized', 403)
     }
+  } catch (error) {
+    throw new ErrorObject(error.message, error.statusCode || 500)
+  }
 }
