4c112c0c0e715ae3026a7f008cd68ce3f4ef7c38
ignacioborraz <ignacioborraz@hotmail.com>
2022-07-21 23:05:33 -0300

    env

--- services/sendMail.js
+++ services/sendMail.js
@@ -1,6 +1,6 @@
 const sgMail = require('@sendgrid/mail')
 
-sgMail.setApiKey(process.env.SECRET_KEY)
+sgMail.setApiKey(process.env.SENDRGRID__KEY)
 
 const { ErrorObject } = require('../helpers/error')
 
