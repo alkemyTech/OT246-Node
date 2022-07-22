af1fbf62656b0ed6af0c409b557aa398a9d77890
ghnoob <ptnchk.rodrigo@gmail.com>
2022-07-22 14:20:28 -0300

    get method added

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,29 @@
 const createHttpError = require('http-errors')
-const { deleteUserBy } = require('../services/users')
+const { getAllUsers, deleteUserBy } = require('../services/users')
 const { catchAsync } = require('../helpers/catchAsync')
 const { endpointResponse } = require('../helpers/success')
 
 module.exports = {
+  get: catchAsync(async (req, res, next) => {
+    try {
+      const resp = await getAllUsers()
+
+      return endpointResponse({
+        res,
+        code: 200,
+        message: 'Users retrieved successfully',
+        body: resp,
+      })
+    } catch (err) {
+      const httpError = createHttpError(
+        err.statusCode,
+        `[Error getting users] - [users - GET]: ${err.message}`,
+      )
+
+      return next(httpError)
+    }
+  }),
+
   destroy: catchAsync(async (req, res, next) => {
     const { id } = req.params
     try {
