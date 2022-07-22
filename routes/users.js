b28bae847ba9174ad98b9514646bdd6d653dacf7
ghnoob <ptnchk.rodrigo@gmail.com>
2022-07-22 14:21:05 -0300

    get endpoint added

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const { destroy } = require('../controllers/users')
+const { get, destroy } = require('../controllers/users')
 
 const router = express.Router()
 
 // example of a route with index controller get function
+router.get('/', get)
 router.delete('/:id', destroy)
 
 module.exports = router
