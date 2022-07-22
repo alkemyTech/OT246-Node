a1fd851524261870b6fd1ade718a9b6b91b65fab
ignacioborraz <ignacioborraz@hotmail.com>
2022-07-21 23:29:33 -0300

    env

--- services/sendMail.js
+++ services/sendMail.js
@@ -1,13 +1,13 @@
 const sgMail = require('@sendgrid/mail')
 
-sgMail.setApiKey(process.env.SENDRGRID__KEY)
+sgMail.setApiKey(process.env.SENDGRID_KEY)
 
 const { ErrorObject } = require('../helpers/error')
 
 exports.sendMail = async (mail, subject, texthtml) => {
   const sandboxMode = false
-  const correo = {
-    from: process.env.FROM,
+  const settings = {
+    from: process.env.SENDGRID_FROM,
     to: mail,
     subject,
     texthtml,
@@ -18,7 +18,7 @@ exports.sendMail = async (mail, subject, texthtml) => {
     },
   }
   try {
-    await sgMail.send(correo)
+    await sgMail.send(settings)
   } catch (err) {
     throw new ErrorObject(err.message, 500)
   }
