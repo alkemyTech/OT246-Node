63a7d4070d4cff192ef300f100b9aea9c3706f0d
ghnoob <ptnchk.rodrigo@gmail.com>
2022-07-22 14:17:39 -0300

    get all users method added

--- services/users.js
+++ services/users.js
@@ -1,6 +1,18 @@
 const { User } = require('../database/models')
 const { ErrorObject } = require('../helpers/error')
 
+exports.getAllUsers = async () => {
+  try {
+    const users = User.findAll({
+      attributes: { exclude: ['password', 'deletedAt'] },
+    })
+
+    return users
+  } catch (err) {
+    throw new ErrorObject(err.message, 500)
+  }
+}
+
 exports.deleteUserBy = async (id) => {
   try {
     const deletedUser = await User.findByPk(id)
