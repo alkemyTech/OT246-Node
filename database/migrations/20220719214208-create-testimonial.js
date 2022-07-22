46438e60320a75d9fb03abe2032dd6878af384dc
María Roberta Mendoza <64031295+trabajoescueladesarrollo@users.noreply.github.com>
2022-07-22 21:51:03 +0200

    [TESTIMONIALS] Testimonials model and migrations OT246-20 (#12)
    * Testimonials model and migrations
    
    * PR Fixes: format
    
    * Fixes lock-Json
    
    * Fixes lock-Json
    
    * Fixes: paranoid:true

---
+++ database/migrations/20220719214208-create-testimonial.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const sequelize = require("sequelize");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Testimonials', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+       
+      },
+      image: {
+        allowNull: false,
+        type: Sequelize.STRING,
+       
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Testimonials');
+  }
+};
