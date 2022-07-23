94953ce57b0a63e19c0ff1cbcd86118125fa6322
Martin Heredia <martinheredia.dev@gmail.com>
2022-07-23 03:47:59 -0300

    created route with patch method, using middleware to verifcate data

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 const express = require('express')
-const { destroy } = require('../controllers/users')
+const { updateBody } = require('../schemas/update')
+const { validateSchema } = require('../middlewares/validations')
+const { destroy, update } = require('../controllers/users')
 
 const router = express.Router()
 
 // example of a route with index controller get function
 router.delete('/:id', destroy)
+router.patch('/:id', validateSchema(updateBody), update)
 
 module.exports = router
