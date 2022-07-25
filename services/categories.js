5ebf0039a88afc27253da777b6e3ff70969f0e8b
ghnoob <ptnchk.rodrigo@gmail.com>
2022-07-25 17:05:54 -0300

    add getCategoryById method

--- services/categories.js
+++ services/categories.js
@@ -11,3 +11,20 @@ exports.getCategories = async () => {
     throw new ErrorObject(err.message, 500)
   }
 }
+
+exports.getCategoryById = async (id) => {
+  try {
+    const result = await Category.findByPk(
+      id,
+      { attributes: { exclude: ['deletedAt'] } },
+    )
+
+    if (!result) {
+      throw new ErrorObject('Category not found', 404)
+    }
+
+    return result
+  } catch (err) {
+    throw new ErrorObject(err.message, err.statusCode || 500)
+  }
+}
