2669167780c62776890cfd25fa2a514112890290
Brenda Álvarez <bgalacho@gmail.com>
2022-07-25 18:44:39 -0300

    Added Amazon S3 file upload support

---
+++ services/amazonS3.js
@@ -0,0 +1,32 @@
+const aws = require('aws-sdk')
+const fs = require('fs')
+const path = require('path')
+
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_PUBLIC_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+})
+
+export function uploadFileContents(fileName, fileContents) {
+  const params = {
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: fileName,
+    Body: fileContents,
+  }
+
+  return new Promise((resolve, reject) => {
+    s3.upload(params, (err, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(data.Location)
+      }
+    })
+  })
+}
+
+export function uploadFilePath(filePath) {
+  const fileContent = fs.readFileSync(filePath)
+  const fileName = path.parse(filePath).base
+  return uploadFileContents(fileName, fileContent)
+}
