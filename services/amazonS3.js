c03df6e62da46c37d0fbc46b205355acc18e944f
Brenda Álvarez <bgalacho@gmail.com>
2022-07-25 18:53:08 -0300

    Added Amazon S3 file delete support

--- services/amazonS3.js
+++ services/amazonS3.js
@@ -16,11 +16,8 @@ export function uploadFileContents(fileName, fileContents) {
 
   return new Promise((resolve, reject) => {
     s3.upload(params, (err, data) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(data.Location)
-      }
+      if (err) reject(err)
+      else resolve(data.Location)
     })
   })
 }
@@ -29,4 +26,18 @@ export function uploadFilePath(filePath) {
   const fileContent = fs.readFileSync(filePath)
   const fileName = path.parse(filePath).base
   return uploadFileContents(fileName, fileContent)
+}
+
+export function deleteFile(key) {
+  const params = {
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: key,
+  }
+
+  return new Promise((resolve, reject) => {
+    s3.deleteObject(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
+  })
 }
