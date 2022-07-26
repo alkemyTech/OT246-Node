c9f1ba8eb3c09b6f79181ccc3d93ce622ccec3eb
Martin Heredia <martinheredia.dev@gmail.com>
2022-07-26 20:48:07 -0300

    restored organization to main version

--- schemas/organization.js
+++ schemas/organization.js
@@ -2,41 +2,30 @@ const organizationBody = {
   name: {
     in: 'body',
     trim: true,
-    isLength: { options: { min: 4 }, errorMessage: 'Name must be at least 4 characters long' },
-    matches: {
-      options: [
-        /^[a-zA-Z ]*$/,
-      ],
-      errorMessage: 'Name must be alphabetic',
-    },
     notEmpty: { errorMessage: 'Must not be empty' },
-    optional: true,
+    isAlphaWithSpace: { errorMessage: 'Must contain alphabetic characters only' },
+    isOptional: true,
   },
   image: {
     in: 'body',
     trim: true,
     notEmpty: { errorMessage: 'Must not be empty' },
     isURL: { errorMessage: 'Must be a valid URL' },
-    optional: true,
+    isOptional: true,
   },
   address: {
     in: 'body',
     trim: true,
     notEmpty: { errorMessage: 'Must not be empty' },
-    matches: {
-      options: [
-        /^[A-Za-z]+ ?[0-9]+$/],
-      errorMessage: 'Must be a valid address',
-    },
-    isLength: { options: { min: 5 }, errorMessage: 'Address must be at least 5 characters long' },
-    optional: true,
+    isAlphanumericWithSpace: { errorMessage: 'Must contain alphabetic characters only' },
+    isOptional: true,
   },
   phone: {
     in: 'body',
     trim: true,
     notEmpty: { errorMessage: 'Must not be empty' },
     isNumeric: { errorMessage: 'Must contain numeric characters only' },
-    optional: true,
+    isOptional: true,
   },
 }
 
