0632282773e497fb4ca83268b6b4f8fe19cb1098
Martin Heredia <martinheredia.dev@gmail.com>
2022-07-26 02:16:48 -0300

    created route and validation schema

--- routes/categories.js
+++ routes/categories.js
@@ -1,9 +1,13 @@
 const { Router } = require('express')
-const { get, getById } = require('../controllers/categories')
+const { get, getById, post } = require('../controllers/categories')
+const { validateSchema } = require('../middlewares/validations')
+const { categoryBody } = require('../schemas/categories')
+const { authUser } = require('../middlewares/authUser')
 
 const router = Router()
 
 router.get('/public', get)
 router.get('/:id', getById)
+router.post('/', authUser, validateSchema(categoryBody), post)
 
 module.exports = router
