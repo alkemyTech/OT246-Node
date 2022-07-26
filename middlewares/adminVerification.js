928248f7436dee886cfa05382304c41208743efe
Tomas <tomasignaciocontreras@hotmail.com>
2022-07-26 19:05:57 -0300

    fixing variable names and logic of middleware

--- middlewares/adminVerification.js
+++ middlewares/adminVerification.js
@@ -1,17 +1,10 @@
-const { User } = require('../database/models/index')
 const { ErrorObject } = require('../helpers/error')
 
-exports.adminVerification = async (req, res, next) => {
+exports.isUserAdmin = async (req, res, next) => {
   try {
     const { id } = req.params
-    const user = await User.findOne({
-      where: {
-        id,
-      },
-      attributes: ['roleId'],
-    })
 
-    if (user && user.roleId === 1) {
+    if (id == 1) {
       next()
     } else {
       throw new ErrorObject('Unauthorized', 403)
