3e83b46dc4212d635e33f3ad47576a6d39fb8c90
ghnoob <ptnchk.rodrigo@gmail.com>
2022-07-27 17:17:08 -0300

    put method added

--- controllers/news.js
+++ controllers/news.js
@@ -1,5 +1,5 @@
 const createHttpError = require('http-errors')
-const { getNewsById } = require('../services/news')
+const { getNewsById, updateNew } = require('../services/news')
 const { catchAsync } = require('../helpers/catchAsync')
 const { endpointResponse } = require('../helpers/success')
 
@@ -18,6 +18,25 @@ module.exports = {
       const httpError = createHttpError(
         err.statusCode,
         `[Error retrieving news] - [news - GET /news/${req.params.id}]: ${err.message}`,
+      )
+      return next(httpError)
+    }
+  }),
+
+  put: catchAsync(async (req, res, next) => {
+    try {
+      const responseBody = await updateNew(req.params.id)
+      return endpointResponse({
+        res,
+        code: 200,
+        status: true,
+        message: 'News updated successfully',
+        body: responseBody,
+      })
+    } catch (err) {
+      const httpError = createHttpError(
+        err.statusCode,
+        `[Error updating news] - [news - PUT /news/${req.params.id}]: ${err.message}`,
       )
       return next(httpError)
     }
