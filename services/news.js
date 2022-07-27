94981aaae7f873812d2918c318bc917c141e0620
ghnoob <ptnchk.rodrigo@gmail.com>
2022-07-27 17:15:22 -0300

    updateNew method added

--- services/news.js
+++ services/news.js
@@ -1,3 +1,4 @@
+const { ForeignKeyConstraintError } = require('sequelize')
 const { New } = require('../database/models')
 const { ErrorObject } = require('../helpers/error')
 
@@ -12,5 +13,30 @@ exports.getNewsById = async (id) => {
     return newsById
   } catch (err) {
     throw new ErrorObject(err.message, err.statusCode)
+  }
+}
+
+exports.updateNew = async (id, {
+  name, content, image, categoryId,
+}) => {
+  try {
+    const news = await this.getNewsById(id)
+
+    news.setAttributes({
+      name,
+      content,
+      image,
+      categoryId,
+    })
+
+    await news.save()
+
+    return news
+  } catch (err) {
+    if (err instanceof ForeignKeyConstraintError) {
+      throw new ErrorObject(`Category with id ${categoryId} does not exist`, 400)
+    }
+
+    throw new ErrorObject(err.message, err.statusCode || 500)
   }
 }
