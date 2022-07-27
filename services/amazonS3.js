6358366e8372102a6ee8343cf0a976cce7050c9d
SamuelOnti0023 <enrissmuelo@gmail.com>
2022-07-26 23:20:12 -0300

    fix: create an S3 client and update dependencies

--- services/amazonS3.js
+++ services/amazonS3.js
@@ -1,43 +1,10 @@
-const aws = require('aws-sdk')
-const fs = require('fs')
-const path = require('path')
+const { S3Client } = require('@aws-sdk/client-s3')
 
-const s3 = new aws.S3({
-  accessKeyId: process.env.AWS_PUBLIC_KEY,
-  secretAccessKey: process.env.AWS_SECRET_KEY,
+exports.s3 = new S3Client({
+  credentials: {
+    accessKeyId: process.env.AWS_PUBLIC_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+  },
+  region: process.env.AWS_REGION,
+  signatureVersion: 'v4',
 })
-
-exports.uploadFileContents = (fileName, fileContents) => {
-  const params = {
-    Bucket: process.env.AWS_BUCKET_NAME,
-    Key: fileName,
-    Body: fileContents,
-  }
-
-  return new Promise((resolve, reject) => {
-    s3.upload(params, (err, data) => {
-      if (err) reject(err)
-      else resolve(data.Location)
-    })
-  })
-}
-
-exports.uploadFilePath = (filePath) => {
-  const fileContent = fs.readFileSync(filePath)
-  const fileName = path.parse(filePath).base
-  return exports.uploadFileContents(fileName, fileContent)
-}
-
-exports.deleteFile = (key) => {
-  const params = {
-    Bucket: process.env.AWS_BUCKET_NAME,
-    Key: key,
-  }
-
-  return new Promise((resolve, reject) => {
-    s3.deleteObject(params, (err, data) => {
-      if (err) reject(err)
-      else resolve(data)
-    })
-  })
-}
