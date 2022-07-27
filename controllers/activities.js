73dedec058e9ce379027ab09c87f0b2025a68d76
Brenda Álvarez <bgalacho@gmail.com>
2022-07-27 16:52:45 -0300

    image added

--- controllers/activities.js
+++ controllers/activities.js
@@ -1,23 +1,25 @@
 const createHttpError = require('http-errors')
-const { createActivity } = require('../services/categories')
+const { createActivity } = require('../services/activities')
 const { catchAsync } = require('../helpers/catchAsync')
 const { endpointResponse } = require('../helpers/success')
 
-catchAsync(async (req, res, next) => {
-  const { body } = req
-  try {
-    const response = await createActivity(body)
-    return endpointResponse({
-      res,
-      code: 201,
-      message: 'Activity was created successfully',
-      body: response,
-    })
-  } catch (err) {
-    const httpError = createHttpError(
-      err.statusCode,
-      `[Error creating activity] - [activities - POST]: ${err.message}`,
-    )
-    return next(httpError)
-  }
-})
+module.exports = {
+  post: catchAsync(async (req, res, next) => {
+    const { name, content, image } = req.body
+    try {
+      const response = await createActivity(name, content, image)
+      return endpointResponse({
+        res,
+        code: 201,
+        message: 'Activity was created successfully',
+        body: response,
+      })
+    } catch (err) {
+      const httpError = createHttpError(
+        err.statusCode,
+        `[Error creating activity] - [activities - POST]: ${err.message}`,
+      )
+      return next(httpError)
+    }
+  }),
+}
--- services/activities.js
+++ services/activities.js
@@ -1,9 +1,9 @@
 const { Activity } = require('../database/models')
 const { ErrorObject } = require('../helpers/error')
 
-exports.createActivity = async (name, content) => {
+exports.createActivity = async (name, content, image) => {
   try {
-    const activity = await Activity.create({ name, content })
+    const activity = await Activity.create({ name, content, image })
     return activity
   } catch (err) {
     throw new ErrorObject(err.message, err.statusCode || 500)
