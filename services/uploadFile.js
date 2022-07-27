421366a855b09ec4316be41f02e0961057cd84c5
SamuelOnti0023 <enrissmuelo@gmail.com>
2022-07-27 02:00:22 -0300

    feat : upload file service

--- services/uploadFile.js
+++ services/uploadFile.js
@@ -1,11 +1,25 @@
 const fs = require('fs')
-const { uploadFileContents } = require('./amazonS3')
+const { s3, createCommand } = require('./amazonS3')
 const { ErrorObject } = require('../helpers/error')
+
+const config = {
+  bucketName: process.env.AWS_BUCKET_NAME,
+  region: process.env.AWS_REGION,
+}
 
 exports.uploadFile = async (tempFilePath) => {
   const content = fs.readFileSync(tempFilePath)
+  const command = createCommand({
+    Bucket: config.bucketName,
+    Key: tempFilePath,
+    Body: content,
+  })
   try {
-    return await uploadFileContents(tempFilePath, content)
+    const { ETag } = await s3.send(command)
+    if (!ETag) {
+      throw new ErrorObject('No file upload', 502)
+    }
+    return `https://${config.bucketName}.s3.${config.region}.amazonaws.com/${tempFilePath}`
   } catch (err) {
     throw new ErrorObject(err.message, 502)
   }
