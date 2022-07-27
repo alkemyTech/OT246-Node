ca4ec65ea3e0fd3949b2f62e8b2af96cb7b5984c
Brenda Álvarez <bgalacho@gmail.com>
2022-07-27 18:22:25 -0300

    Migration & Model Contacts

--- database/models/contact.js
+++ database/models/contact.js
@@ -5,25 +5,22 @@ const {
 module.exports = (sequelize, DataTypes) => {
   class Contact extends Model {
     /**
-       * Helper method for defining associations.
-       * This method is not a part of Sequelize lifecycle.
-       * The `models/index` file will call this method automatically.
-       */
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
     static associate(models) {
-
+      // define association here
     }
   }
-
   Contact.init({
     name: DataTypes.STRING,
     phone: DataTypes.INTEGER,
-    email: DataTypes.STRING,
     message: DataTypes.STRING,
-    deletedAt: DataTypes.DATE,
+    email: DataTypes.STRING,
+    deleteAt: DataTypes.DATE,
   }, {
     sequelize,
-    timestamps: true,
-    paranoid: true,
     modelName: 'Contact',
   })
   return Contact
