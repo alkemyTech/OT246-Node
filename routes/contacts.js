ae06036fb5e08b6b1edf05cbb2bab3b0483b12a8
SamuelOnti0023 <enrissmuelo@gmail.com>
2022-07-28 16:44:50 -0300

    add: isUserAdmin to router.get contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,9 +1,10 @@
 const { Router } = require('express')
 const { get } = require('../controllers/contacts')
 const { authUser } = require('../middlewares/authUser')
+const { isUserAdmin } = require('../middlewares/adminVerification')
 
 const router = Router()
 
-router.get('/', authUser, get)
+router.get('/', authUser, isUserAdmin, get)
 
 module.exports = router
