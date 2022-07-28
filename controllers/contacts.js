1f1e2515be9763d07948dfbce16140939d7a3d24
SamuelOnti0023 <enrissmuelo@gmail.com>
2022-07-28 18:18:21 -0300

    feat: integrate send mail service when someone register your contact method

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,6 +2,7 @@ const createHttpError = require('http-errors')
 const { getContacts } = require('../services/contacts')
 const { catchAsync } = require('../helpers/catchAsync')
 const { endpointResponse } = require('../helpers/success')
+// const { sendMailRegistrationContact } = require('../services/sendMail')
 
 module.exports = {
   get: catchAsync(async (req, res, next) => {
@@ -20,6 +21,30 @@ module.exports = {
         `[Error retrieving contacts] - [contacts - GET]: ${err.message}`,
       )
       return next(httpError)
+    }
+  }),
+  post: catchAsync(async (req, res, next) => {
+    try {
+      // const contact = await algo ...
+
+      // a Descomentar
+      // await sendMailRegistrationContact(contact.email, {
+      //   'name': contact.name,
+      //   'email': contact.email,
+      //   'phone': contact.phone,
+      // })
+      endpointResponse({
+        res,
+        code: 201,
+        message: 'Contact created successfully',
+        // body: responseBody,
+      })
+    } catch (err) {
+      const httpError = createHttpError(
+        err.statusCode,
+        `[Error creating contact] - [contacts - POST]: ${err.message}`,
+      )
+      next(httpError)
     }
   }),
 }
