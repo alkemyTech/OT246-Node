b3c6602944132f130de2de5fb3ada6781ec27451
ignacioborraz <ignacioborraz@hotmail.com>
2022-07-27 22:52:30 -0300

    slint

--- schemas/news.js
+++ schemas/news.js
@@ -7,15 +7,12 @@ const newBody = {
   content: {
     in: 'body',
     trim: true,
-<<<<<<< HEAD
-=======
     matches: {
       options: [
         /^[-_ a-zA-Z0-9]+$/,
       ],
       errorMessage: 'Content must be alphanumeric',
     },
->>>>>>> 58b6c23d7748500622c9908116d8c6994750bc5f
     notEmpty: { errorMessage: 'Must not be empty' },
   },
   image: {
@@ -24,14 +21,11 @@ const newBody = {
     notEmpty: { errorMessage: 'Must not be empty' },
     isURL: { errorMessage: 'Must be a valid URL' },
   },
-<<<<<<< HEAD
-=======
   categoryId: {
     in: 'body',
     trim: true,
     isInt: true,
     notEmpty: { errorMessage: 'Must not be empty' },
   },
->>>>>>> 58b6c23d7748500622c9908116d8c6994750bc5f
 }
 module.exports = { newBody }
