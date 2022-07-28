77c13ee69412af87e4199fcf93b1d0f71532b923
Brenda Álvarez Galacho <brendagalacho@gmail.com>
2022-07-28 12:45:32 -0300

    [Models] Contact model & migration (#49)
    * Create Activity (services)
    
    * Create activity (controllers)
    
    * image added
    
    * Endpoint Activities Create
    
    * Contact Model Created
    
    * Migration & Model Contacts
    
    Co-authored-by: Brenda Álvarez <bgalacho@gmail.com>

---
+++ database/migrations/20220727211550-create-contact.js
@@ -0,0 +1,42 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Contacts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      phone: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      message: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      deleteAt: {
+        type: Sequelize.DATE,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    })
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Contacts')
+  },
+}
