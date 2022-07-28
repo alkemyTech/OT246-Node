f74bc4db30b3b573ea9a8b34583b8264bf83d5f0
Rodrigo Pietnechuk <ptnchk.rodrigo@gmail.com>
2022-07-28 11:21:45 -0300

    [ENDPOINT] PUT /news/:id OT246-48 (#48)

--- routes/news.js
+++ routes/news.js
@@ -1,11 +1,17 @@
 const router = require('express').Router()
-const { getById, post, destroy } = require('../controllers/news')
+const {
+  getById,
+  post,
+  put,
+  destroy,
+} = require('../controllers/news')
 const { authUser } = require('../middlewares/authUser')
 const { validateSchema } = require('../middlewares/validations')
-const { newBody } = require('../schemas/news')
+const { newBodyPost, newBodyPut } = require('../schemas/news')
 
 router.get('/:id', authUser, getById)
-router.post('/', authUser, validateSchema(newBody), post)
+router.post('/', authUser, validateSchema(newBodyPost), post)
+router.put(':/id', authUser, validateSchema(newBodyPut), put)
 router.delete('/:id', authUser, destroy)
 
 module.exports = router
--- services/news.js
+++ services/news.js
@@ -1,3 +1,4 @@
+const { ForeignKeyConstraintError } = require('sequelize')
 const { New, Category } = require('../database/models')
 const { ErrorObject } = require('../helpers/error')
 
@@ -36,6 +37,31 @@ exports.createNew = async (data) => {
     })
     return result
   } catch (err) {
+    throw new ErrorObject(err.message, err.statusCode || 500)
+  }
+}
+
+exports.updateNew = async (id, {
+  name, content, image, categoryId,
+}) => {
+  try {
+    const news = await this.getNewsById(id)
+
+    news.setAttributes({
+      name,
+      content,
+      image,
+      categoryId,
+    })
+
+    await news.save()
+
+    return news
+  } catch (err) {
+    if (err instanceof ForeignKeyConstraintError) {
+      throw new ErrorObject('Category not found', 404)
+    }
+
     throw new ErrorObject(err.message, err.statusCode || 500)
   }
 }
