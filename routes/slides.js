15d0464b5dcd10e2c665f199825a41f1af1e1da0
María Roberta Mendoza <64031295+MRobertaMendoza@users.noreply.github.com>
2022-07-29 01:20:22 +0200

    [Endpoint] List the Slides OT246-57 (#50)
    * Endpoint list the Slides
    
    * Fix Changes
    
    * Fix changes
    
    * Fix changes
    
    * Fix changes
    
    * Delete bash.exe.stackdump
    
    fix changes

--- routes/slides.js
+++ routes/slides.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
-const { getById } = require('../controllers/slides')
+const { getById, getAll } = require('../controllers/slides')
 
 router.get('/:id', getById)
+router.get('/', getAll)
 
 module.exports = router
--- services/slides.js
+++ services/slides.js
@@ -12,3 +12,15 @@ exports.getSlideById = async (id) => {
     throw new ErrorObject(err.message, 404)
   }
 }
+
+exports.getSlideAll = async () => {
+  try {
+    const slide = await Slide.findAll({
+      attributes: { exclude: ['text', 'organizationId', 'id'] },
+    })
+
+    return slide
+  } catch (err) {
+    throw new ErrorObject(err.message, err.statusCode)
+  }
+}
