41232b69fd19ff3f6ad1b0f38b15fbe351db6503
Brenda Álvarez Galacho <brendagalacho@gmail.com>
2022-07-29 14:43:32 -0300

    Update slides.js

--- routes/slides.js
+++ routes/slides.js
@@ -1,12 +1,13 @@
 const router = require('express').Router()
-const { getById, getAll, put, destroy 
-  } = require('../controllers/slides')
+const {
+  getById, getAll, put, destroy,
+} = require('../controllers/slides')
 const { authUser } = require('../middlewares/authUser')
 const { isUserAdmin } = require('../middlewares/adminVerification')
 
 router.get('/:id', getById)
 router.get('/', getAll)
-router.delete('/:id', authUser, isUserAdmin, destroy)
 router.put('/:id', authUser, isUserAdmin, put)
+router.delete('/:id', authUser, isUserAdmin, destroy)
 
 module.exports = router
