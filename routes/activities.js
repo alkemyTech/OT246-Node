205fded8a2fdee418cfdc08b680e8ab0ff28356b
Tomas <tomasignaciocontreras@hotmail.com>
2022-07-30 16:35:13 -0300

    added middleware userAdmin on route

--- routes/activities.js
+++ routes/activities.js
@@ -6,10 +6,11 @@ const {
 const { validateSchema } = require('../middlewares/validations')
 const { activityBodyPost, activityBodyPut } = require('../schemas/activities')
 const { authUser } = require('../middlewares/authUser')
+const { isUserAdmin } = require('../middlewares/adminVerification')
 
 const router = Router()
 
 router.post('/', authUser, validateSchema(activityBodyPost), post)
-router.put('/:id', authUser, validateSchema(activityBodyPut), put)
+router.put('/:id', authUser, isUserAdmin, validateSchema(activityBodyPut), put)
 
 module.exports = router
