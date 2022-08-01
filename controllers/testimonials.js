9b6611417b9a4dc91a11bad4ef7b279dc90f7d47
Samuel Ontiveros <38141029+Samuel0023@users.noreply.github.com>
2022-08-01 17:11:55 -0300

    [ENDPOINT] Endpoint Update Testimonials OT246-70 (#68)
    * feat: create testimonials controller
    
    * feat: update testimonials route
    
    * feat: add Testimonials Router to index
    
    * feat: create update testimonial
    
    * feat: testimonials schema to validate the endpoint
    
    * feat: update the handling of middlwares in testimonialsRouter
    
    * fix: remove the functionalities defined out of scope

--- controllers/testimonials.js
+++ controllers/testimonials.js
@@ -1,9 +1,27 @@
 const createHttpError = require('http-errors')
-const { createTestimonial } = require('../services/testimonials')
+const { createTestimonial, updateTestimonial } = require('../services/testimonials')
 const { catchAsync } = require('../helpers/catchAsync')
 const { endpointResponse } = require('../helpers/success')
 
 module.exports = {
+  put: catchAsync(async (req, res, next) => {
+    try {
+      const testimonial = await updateTestimonial(req.params.id, req.body)
+      return endpointResponse({
+        res,
+        code: 200,
+        status: true,
+        message: 'Testimonial updated successfully',
+        body: testimonial,
+      })
+    } catch (err) {
+      const httpError = createHttpError(
+        err.statusCode,
+        `[Error updating testimonial] - [testimonials - PUT]: ${err.message}`,
+      )
+      return next(httpError)
+    }
+  }),
   post: catchAsync(async (req, res, next) => {
     const { body } = req
     try {
