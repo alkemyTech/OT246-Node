8a007a94fa7829ef814d364c5ec960c95b8cab03
Martin Heredia <martinheredia.dev@gmail.com>
2022-08-01 00:48:21 -0300

    fixed validations to post body

--- schemas/testimonials.js
+++ schemas/testimonials.js
@@ -7,8 +7,8 @@ const testimonialBody = {
       errorMessage: 'Name must be at least 4 characters long',
     },
     matches: {
-      options: [/^[A-Za-z0-9? ,_-]+$$/],
-      errorMessage: 'Name must be alphabetic',
+      options: [/^[A-Za-z0-9? ,_-]+$/],
+      errorMessage: 'Name must be alphanumeric',
     },
     notEmpty: { errorMessage: 'Must not be empty' },
   },
@@ -19,18 +19,13 @@ const testimonialBody = {
       options: { min: 5 },
       errorMessage: 'Name must be at least 4 characters long',
     },
-    matches: {
-      options: [/^[A-Za-z0-9? ,_-]+$/],
-      errorMessage: 'Content must be alphabetic',
-    },
     notEmpty: { errorMessage: 'Must not be empty' },
   },
   image: {
     in: 'body',
     trim: true,
-    notEmpty: { errorMessage: 'Must not be empty' },
-    isURL: { errorMessage: 'Must be a valid URL' },
     optional: true,
+    isURL: { errorMessage: 'Must be a valid URL' },
   },
 }
 
