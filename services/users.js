816b7b17ea0578777e3ac204f31099a060a5f3c2
ghnoob <ptnchk.rodrigo@gmail.com>
2022-08-01 21:29:57 -0300

    dont include roleId in token

--- services/users.js
+++ services/users.js
@@ -105,7 +105,7 @@ exports.loginUser = async ({ email, password }) => {
     if (!user || !bcrypt.compareSync(password, user.password)) {
       throw new ErrorObject('Invalid Credentials ', 401)
     }
-    const token = generateToken({ email: user.dataValues.email, roleId: user.dataValues.roleId })
+    const token = generateToken({ email: user.dataValues.email })
     return token
   } catch (error) {
     throw new ErrorObject(error.message, error.statusCode || 500)
