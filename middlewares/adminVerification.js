7bcc7d813cd7eac0c898cb3fdbe67fe9ca73dede
ghnoob <ptnchk.rodrigo@gmail.com>
2022-08-01 22:19:55 -0300

    use authUser along with isUserAdmin

--- middlewares/adminVerification.js
+++ middlewares/adminVerification.js
@@ -1,15 +1,13 @@
 const { ErrorObject } = require('../helpers/error')
+const { authUser } = require('./authUser')
 
-exports.isUserAdmin = (req, res, next) => {
-  try {
-    const { roleId } = req.user
-
-    if (roleId === 1) {
+exports.isUserAdmin = [
+  authUser,
+  (req, res, next) => {
+    if (req.user && req.user.roleId === 1) {
       next()
     } else {
       throw new ErrorObject('This endpoint is for admins only', 403)
     }
-  } catch (error) {
-    throw new ErrorObject(error.message, error.statusCode || 500)
-  }
-}
+  },
+]
