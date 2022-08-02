0e0fcd6e6161f9a44a4199f0a8ca589539a0a0e0
ghnoob <ptnchk.rodrigo@gmail.com>
2022-08-01 21:58:24 -0300

    modify authUser to use req.user

--- middlewares/authUser.js
+++ middlewares/authUser.js
@@ -1,29 +1,19 @@
 const createHttpError = require('http-errors')
-const { verifyToken } = require('./jwt')
-const { endpointResponse } = require('../helpers/success')
-const { catchAsync } = require('../helpers/catchAsync')
+const { setUserInRequest } = require('./setUserInRequest')
 
 module.exports = {
-  authUser: catchAsync(async (req, res, next) => {
-    try {
-      if (!req.headers.authorization) {
-        endpointResponse({
-          res,
-          code: 403,
-          status: false,
-          message: 'You are not authorized to access this resource',
-        })
-      } else {
-        const token = req.headers.authorization.split(' ')[1]
-        req.user = verifyToken(token)
+  authUser: [
+    setUserInRequest,
+    (req, res, next) => {
+      if (req.user) {
         next()
+      } else {
+        const httpError = createHttpError(
+          401,
+          'You must be logged in to access this resource',
+        )
+        next(httpError)
       }
-    } catch (err) {
-      const httpError = createHttpError(
-        401,
-        `[Error authenticating user] - [authUser - middleware] - ${err.message}`,
-      )
-      next(httpError)
-    }
-  }),
+    },
+  ],
 }
