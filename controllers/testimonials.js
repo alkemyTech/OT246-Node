4fe4f89c2ec23ee6c3fcb486af10abe7e4f33825
SamuelOnti0023 <enrissmuelo@gmail.com>
2022-08-02 08:59:06 -0300

    fix: standarize testimonials responses

--- controllers/testimonials.js
+++ controllers/testimonials.js
@@ -5,19 +5,20 @@ const { endpointResponse } = require('../helpers/success')
 
 module.exports = {
   put: catchAsync(async (req, res, next) => {
+    const { body, params: { id } } = req
     try {
-      const testimonial = await updateTestimonial(req.params.id, req.body)
+      const responseBody = await updateTestimonial(id, body)
       return endpointResponse({
         res,
         code: 200,
         status: true,
         message: 'Testimonial updated successfully',
-        body: testimonial,
+        body: responseBody,
       })
     } catch (err) {
       const httpError = createHttpError(
         err.statusCode,
-        `[Error updating testimonial] - [testimonials - PUT]: ${err.message}`,
+        `[Error updating testimonial] - [testimonials/${id} - PUT]: ${err.message}`,
       )
       return next(httpError)
     }
@@ -25,12 +26,12 @@ module.exports = {
   post: catchAsync(async (req, res, next) => {
     const { body } = req
     try {
-      const response = await createTestimonial(body)
+      const responseBody = await createTestimonial(body)
       return endpointResponse({
         res,
         code: 201,
         message: 'Testimonial created successfully',
-        body: response,
+        body: responseBody,
       })
     } catch (err) {
       const httpError = createHttpError(
