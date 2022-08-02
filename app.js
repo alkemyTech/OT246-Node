c498e4f06797a1fd13450c7fca2d8b84a763bc73
ignacioborraz <ignacioborraz@hotmail.com>
2022-08-02 10:03:06 -0300

    fixed 'middleware'

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const cors = require('cors')
-const fileUpload = require('express-fileupload')
 require('dotenv').config()
 
 const indexRouter = require('./routes/index')
@@ -23,7 +22,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(fileUpload())
 
 app.use('/', indexRouter)
 
