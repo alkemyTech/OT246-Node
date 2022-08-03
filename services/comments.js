a4f501e754a002ed6698a69de326b77e7cdd055d
ghnoob <ptnchk.rodrigo@gmail.com>
2022-08-03 20:47:19 -0300

    comments service & createComment method added

---
+++ services/comments.js
@@ -0,0 +1,17 @@
+const { ForeignKeyConstraintError } = require('sequelize')
+const { Comment } = require('../database/models')
+const { ErrorObject } = require('../helpers/error')
+
+exports.createComment = async ({ userId, newId, body }) => {
+  try {
+    const comment = await Comment.create({ userId, newId, body })
+
+    return comment
+  } catch (err) {
+    if (err instanceof ForeignKeyConstraintError) {
+      throw new ErrorObject('New not found', 404)
+    }
+
+    throw new ErrorObject(err.message, 500)
+  }
+}
