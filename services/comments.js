f68a08272802bb337660e514614f8fa6cb295298
SamuelOnti0023 <enrissmuelo@gmail.com>
2022-08-03 19:40:12 -0300

    feat: get all comments sorted by creation date (service)

---
+++ services/comments.js
@@ -0,0 +1,15 @@
+const { Comment } = require('../database/models')
+const { ErrorObject } = require('../helpers/error')
+
+exports.getComments = async () => {
+  try {
+    const comments = await Comment.findAll({
+      order: ['createdAt', 'DESC'],
+      attributes: ['body'],
+    })
+
+    return comments
+  } catch (err) {
+    throw new ErrorObject(err.message, 500)
+  }
+}
