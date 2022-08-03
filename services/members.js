bdd1e3af90385e919c19a8a999ca9af4c4d29154
SamuelOnti0023 <enrissmuelo@gmail.com>
2022-08-03 02:06:31 -0300

    feat: deleteMember service

--- services/members.js
+++ services/members.js
@@ -12,3 +12,16 @@ exports.createMember = async ({ name, image }) => {
     throw new ErrorObject(err.message, err.statusCode || 500)
   }
 }
+
+exports.deleteMember = async (id) => {
+  try {
+    const member = await Member.findByPk(id)
+    if (!member) {
+      throw new ErrorObject('Member not found', 404)
+    }
+    await member.destroy()
+    return member
+  } catch (err) {
+    throw new ErrorObject(err.message, 500)
+  }
+}
