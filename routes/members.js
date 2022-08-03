2e53445f132256a177dfea83ac9e90b5b7c8e763
ghnoob <ptnchk.rodrigo@gmail.com>
2022-08-03 11:02:56 -0300

    PUT /members/:id endpoint added

--- routes/members.js
+++ routes/members.js
@@ -1,10 +1,11 @@
 const router = require('express').Router()
-const { post } = require('../controllers/members')
+const { post, put } = require('../controllers/members')
 const { validateSchema } = require('../middlewares/validations')
 const { memberBody } = require('../schemas/members')
 const { authUser } = require('../middlewares/authUser')
 const { isUserAdmin } = require('../middlewares/adminVerification')
 
 router.post('/', authUser, isUserAdmin, validateSchema(memberBody), post)
+router.put('/:id', authUser, put)
 
 module.exports = router
