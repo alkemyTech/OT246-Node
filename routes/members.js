d0a4f732b0f6793b782eb17c35b4dde42e4b8025
SamuelOnti0023 <enrissmuelo@gmail.com>
2022-08-03 02:07:08 -0300

    feat: endpoint into members router

--- routes/members.js
+++ routes/members.js
@@ -1,10 +1,10 @@
 const router = require('express').Router()
-const { post } = require('../controllers/members')
+const { post, destroy } = require('../controllers/members')
 const { validateSchema } = require('../middlewares/validations')
 const { memberBody } = require('../schemas/members')
 const { authUser } = require('../middlewares/authUser')
 const { isUserAdmin } = require('../middlewares/adminVerification')
 
 router.post('/', authUser, isUserAdmin, validateSchema(memberBody), post)
-
+router.delete('/:id', authUser, destroy)
 module.exports = router
