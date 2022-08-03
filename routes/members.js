3b2e95107cd73b3eccb66321f2b8ccd4542e0f75
SamuelOnti0023 <enrissmuelo@gmail.com>
2022-08-02 23:33:51 -0300

    resolve: eslint

--- routes/members.js
+++ routes/members.js
@@ -3,7 +3,7 @@ const { post } = require('../controllers/members')
 const { validateSchema } = require('../middlewares/validations')
 const { memberBody } = require('../schemas/members')
 const { authUser } = require('../middlewares/authUser')
-const { isUserAdmin } = require('../middlewares/adminVerification')
+const { isUserAdmin } = require('../middlewares/authUserAdmin')
 
 router.post('/', authUser, isUserAdmin, validateSchema(memberBody), post)
 
