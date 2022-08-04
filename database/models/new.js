7d6b605ca9859a913423964b8ead24e1fbb184fb
Brenda Álvarez <bgalacho@gmail.com>
2022-08-04 01:21:26 -0300

    Fix

--- database/models/new.js
+++ database/models/new.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       New.belongsTo(models.Category, { foreignKey: 'categoryId' })
-      New.hasmany(models.Comment, { foreignKey: 'newsId' })
+      New.hasMany(models.Comment, { foreignKey: 'newsId' })
     }
   }
   New.init({
