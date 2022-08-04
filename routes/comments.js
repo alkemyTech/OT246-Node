2eb2f93f0d366ec4cad12c8f0c1f6477d658101f
ghnoob <ptnchk.rodrigo@gmail.com>
2022-08-04 16:11:39 -0300

    fix variable name

--- routes/comments.js
+++ routes/comments.js
@@ -2,10 +2,10 @@ const { Router } = require('express')
 const { post } = require('../controllers/comments')
 const { authUser } = require('../middlewares/authUser')
 const { validateSchema } = require('../middlewares/validations')
-const { commentsBodyPost } = require('../schemas/comments')
+const { commentBody } = require('../schemas/comments')
 
 const router = Router()
 
-router.post('/', authUser, validateSchema(commentsBodyPost), post)
+router.post('/', authUser, validateSchema(commentBody), post)
 
 module.exports = router
