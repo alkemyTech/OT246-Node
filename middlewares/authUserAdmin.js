98421cbd8415624daa90efbd690828669ee75d51
Tomas <tomasignaciocontreras@hotmail.com>
2022-08-03 23:06:39 -0300

    resolve middleware conflict

---
+++ middlewares/authUserAdmin.js
@@ -0,0 +1,13 @@
+const { ErrorObject } = require('../helpers/error')
+const { authUser } = require('./authUser')
+
+exports.authUserAdmin = [
+  authUser,
+  (req, res, next) => {
+    if (req.user && req.user.roleId === 1) {
+      next()
+    } else {
+      throw new ErrorObject('This endpoint is for admins only', 403)
+    }
+  },
+]
