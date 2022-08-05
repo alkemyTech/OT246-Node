ebdf6adda526d40a70711665a4acea7381a33f43
Rodrigo Pietnechuk <ptnchk.rodrigo@gmail.com>
2022-08-05 10:45:03 -0300

    [ENDPOINT] POST /comments OT246-81 (#87)

--- services/comments.js
+++ services/comments.js
@@ -1,3 +1,4 @@
+const { ForeignKeyConstraintError } = require('sequelize')
 const { Comment } = require('../database/models')
 const { ErrorObject } = require('../helpers/error')
 
@@ -10,6 +11,20 @@ exports.getComments = async () => {
 
     return comments
   } catch (err) {
+    throw new ErrorObject(err.message, 500)
+  }
+}
+
+exports.createComment = async ({ userId, newsId, body }) => {
+  try {
+    const comment = await Comment.create({ userId, newsId, body })
+
+    return comment
+  } catch (err) {
+    if (err instanceof ForeignKeyConstraintError) {
+      throw new ErrorObject('New not found', 404)
+    }
+
     throw new ErrorObject(err.message, 500)
   }
 }
