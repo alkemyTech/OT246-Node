087a938dfc909dc9b35646774eeb384f2dc0cb24
ghnoob <ptnchk.rodrigo@gmail.com>
2022-08-05 14:16:36 -0300

    GET /testimonials endpoint added

--- routes/testimonials.js
+++ routes/testimonials.js
@@ -1,9 +1,15 @@
 const router = require('express').Router()
-const { post, put, destroy } = require('../controllers/testimonials')
+const {
+  post,
+  put,
+  destroy,
+  get,
+} = require('../controllers/testimonials')
 const { authUserAdmin } = require('../middlewares/authUserAdmin')
 const { validateSchema } = require('../middlewares/validations')
 const { testimonialBody } = require('../schemas/testimonials')
 
+router.get('/', get)
 router.post('/', authUserAdmin, validateSchema(testimonialBody), post)
 router.put('/:id', authUserAdmin, put)
 router.delete('/:id', authUserAdmin, destroy)
