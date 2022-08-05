c28870d72d62cae834aa2437127987ad06f0aebc
Tomas <tomasignaciocontreras@hotmail.com>
2022-08-05 15:45:01 -0300

    added service, controller and route

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,7 +1,7 @@
 const createHttpError = require('http-errors')
 const { catchAsync } = require('../helpers/catchAsync')
 const { endpointResponse } = require('../helpers/success')
-const { getComments, createComment } = require('../services/comments')
+const { getComments, createComment, updateComment } = require('../services/comments')
 
 module.exports = {
   get: catchAsync(async (req, res, next) => {
@@ -43,6 +43,27 @@ module.exports = {
       )
 
       next(httpError)
+    }
+  }),
+
+  put: catchAsync(async (req, res, next) => {
+    const { user: { id: userId }, body: { body }, params: { id } } = req
+    try {
+      const response = await updateComment(id, body, userId)
+
+      return endpointResponse({
+        res,
+        code: 200,
+        status: true,
+        message: 'Comment updated successfully',
+        body: response,
+      })
+    } catch (err) {
+      const httpError = createHttpError(
+        err.statusCode,
+        `[Error updating comment] - [comment/${id} - PUT]: ${err.message}`,
+      )
+      return next(httpError)
     }
   }),
 }
--- routes/comments.js
+++ routes/comments.js
@@ -1,13 +1,14 @@
 const { Router } = require('express')
-const { get, post } = require('../controllers/comments')
+const { get, post, put } = require('../controllers/comments')
 const { authUser } = require('../middlewares/authUser')
 const { authUserAdmin } = require('../middlewares/authUserAdmin')
 const { validateSchema } = require('../middlewares/validations')
-const { commentBodyPost } = require('../schemas/comments')
+const { commentBodyPost, commentBodyPut } = require('../schemas/comments')
 
 const router = Router()
 
 router.get('/', authUserAdmin, get)
 router.post('/', authUser, validateSchema(commentBodyPost), post)
+router.put('/:id', authUser, validateSchema(commentBodyPut), put)
 
 module.exports = router
