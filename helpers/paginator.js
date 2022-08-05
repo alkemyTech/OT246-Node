02e162e60abc7de8ac70f4724c4acf12fb4848ad
Samuel Ontiveros <38141029+Samuel0023@users.noreply.github.com>
2022-08-05 11:58:32 -0300

    [ENDPOINT] Add Pagination to News OT246-86 (#90)
    * feat: destroy methon into members controller
    
    * feat: deleteMember service
    
    * feat: endpoint into members router
    
    * feat: create Paginator class
    
    * feat: add base url in config.js
    
    * feat: add get function in news controller
    
    * feat: add getNewsPaginated service
    
    * feat: add get/news route in news router
    
    * feat: add BASE_URL to parms.env
    
    * Update config.js

---
+++ helpers/paginator.js
@@ -0,0 +1,48 @@
+class Paginator {
+  constructor(idPage, context, cant) {
+    this.idPage = Number(idPage)
+    this.limit = 10
+    this.context = context
+    this.setCantPages(cant)
+  }
+
+  setCantPages(cant) {
+    this.cantPages = Number((cant / 10).toFixed())
+  }
+
+  getOffset() {
+    if (this.idPage <= 0) {
+      return 0
+    }
+    return (this.idPage) * 10
+  }
+
+  getLimit() {
+    return this.limit
+  }
+
+  prevUrl(baseURL) {
+    if (this.idPage > 0) {
+      return `${baseURL}/${this.context}/?page=${this.idPage - 1}`
+    }
+    return ''
+  }
+
+  nextUrl(baseURL) {
+    if ((this.cantPages - this.idPage) > 0) {
+      return `${baseURL}/${this.context}/?page=${this.idPage + 1}`
+    }
+    return ''
+  }
+
+  getAttachedUrl() {
+    const baseURL = process.env.BASE_URL
+    return { prevUrl: this.prevUrl(baseURL), nextUrl: this.nextUrl(baseURL) }
+  }
+
+  getParams() {
+    return { offset: this.getOffset(), limit: this.getLimit() }
+  }
+}
+
+module.exports = Paginator
