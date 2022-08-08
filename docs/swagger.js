16ddf4f25492c2a9bf525377245fbf9b9eb95ed2
Maria Roberta Mendoza <trabajoescueladesarrollo@gmail.com>
2022-08-09 01:11:50 +0200

    swapper interface

---
+++ docs/swagger.js
@@ -0,0 +1,20 @@
+// middlewaress, le paso la ruta para poder consumir la API
+
+module.exports = {
+  definition: {
+    openapi: '3.0.3',
+    info: {
+      title: 'ONG api',
+      version: '1.3.0',
+      description: 'Api de la aceleracion de Alkemy',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3001',
+      },
+    ],
+  },
+  apis: ['./routes/*.js',
+    './controllers/*.js',
+    './docs/*.js'],
+}
