25dac0612e52da840418bf97e4c6dd54008c50dc
Rodrigo Pietnechuk <ptnchk.rodrigo@gmail.com>
2022-08-09 16:39:42 -0300

    prevent access to guests

--- routes/testimonials.js
+++ routes/testimonials.js
@@ -5,11 +5,12 @@ const {
   destroy,
   get,
 } = require('../controllers/testimonials')
+const { authUser } = require('../middlewares/authUser')
 const { authUserAdmin } = require('../middlewares/authUserAdmin')
 const { validateSchema } = require('../middlewares/validations')
 const { testimonialBody } = require('../schemas/testimonials')
 
-router.get('/', get)
+router.get('/', authUser, get)
 router.post('/', authUserAdmin, validateSchema(testimonialBody), post)
 router.put('/:id', authUserAdmin, put)
 router.delete('/:id', authUserAdmin, destroy)
