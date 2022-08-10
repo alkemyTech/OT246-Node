dd49aa6a228e008823290204ada684e147b0c8b6
ghnoob <ptnchk.rodrigo@gmail.com>
2022-08-10 16:04:10 -0300

    POST /auth/register responses documented

--- controllers/users.js
+++ controllers/users.js
@@ -35,6 +35,58 @@ module.exports = {
     }
   }),
 
+  /**
+   * @swagger
+   * components:
+   *   responses:
+   *     UserRegistered:
+   *       description: The account was registered successfully
+   *       content:
+   *         application/json:
+   *           schema:
+   *             allOf:
+   *               - $ref: '#/components/schemas/SuccessResponse'
+   *               - type: object
+   *                 properties:
+   *                   code:
+   *                     example: 201
+   *                   message:
+   *                     example: Account registered successfully
+   *                   body:
+   *                     type: object
+   *                     properties:
+   *                       user:
+   *                         $ref: '#/components/schemas/User'
+   *                       token:
+   *                         type: string
+   *                         description: User's login token
+   *                         example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.\
+   *                           eyJlbWFpbCI6Imp1YW4ucGVyZXpAY29tcGFueS5jb20iLCJpYXQiOjE2NTk5Nzg1MDgs\
+   *                           ImV4cCI6MTY1OTk4MjEwOH0.\
+   *                           0vApbsxQ6vqbumair2kFs58JuMX470XPrQ6nDd7BwO4"
+   *     RegisterBadRequest:
+   *       description: The request body validation failed or the provided email is already in use
+   *       content:
+   *         application/json:
+   *           schema:
+   *             allOf:
+   *             - $ref: '#/components/schemas/ValidationError'
+   *             - type: object
+   *               properties:
+   *                 errors:
+   *                   example:
+   *                     - value: notAnEmail
+   *                       msg: Must be an email
+   *                       param: email
+   *                       location: body
+   *         text/html:
+   *           schema:
+   *             type: string
+   *             example: >-
+   *               BadRequestError:
+   *               [Error registering account] - [auth/register - POST]: Email is already in use
+   */
+
   register: catchAsync(async (req, res, next) => {
     const {
       body: {
