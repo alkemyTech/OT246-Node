a5568abb7c2f2bf42ce62378f89cefaab71d7425
ghnoob <ptnchk.rodrigo@gmail.com>
2022-08-11 23:43:16 -0300

    move docs in controller to auth router file

--- controllers/users.js
+++ controllers/users.js
@@ -35,49 +35,6 @@ module.exports = {
     }
   }),
 
-  /**
-   * @swagger
-   * components:
-   *   responses:
-   *     UserRegistered:
-   *       description: The account was registered successfully
-   *       content:
-   *         application/json:
-   *           schema:
-   *             allOf:
-   *               - $ref: '#/components/schemas/SuccessResponse'
-   *               - type: object
-   *                 properties:
-   *                   code:
-   *                     example: 201
-   *                   message:
-   *                     example: Account registered successfully
-   *                   body:
-   *                     type: object
-   *                     properties:
-   *                       user:
-   *                         $ref: '#/components/schemas/User'
-   *                       token:
-   *                         type: string
-   *                         description: User's login token
-   *                         example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.\
-   *                           eyJlbWFpbCI6Imp1YW4ucGVyZXpAY29tcGFueS5jb20iLCJpYXQiOjE2NTk5Nzg1MDgs\
-   *                           ImV4cCI6MTY1OTk4MjEwOH0.\
-   *                           0vApbsxQ6vqbumair2kFs58JuMX470XPrQ6nDd7BwO4"
-   *     RegisterBadRequest:
-   *       allOf:
-   *         - $ref: '#/components/responses/LoginValidationError'
-   *         - description: >-
-   *             The request body validation failed or the provided email is already in use
-   *           content:
-   *             text/html:
-   *               schema:
-   *                 type: string
-   *                 example: >-
-   *                   BadRequestError:
-   *                   [Error registering account] - [auth/register - POST]: Email is already in use
-   */
-
   register: catchAsync(async (req, res, next) => {
     const {
       body: {
@@ -154,57 +111,6 @@ module.exports = {
     }
   }),
 
-  /**
-   * @swagger
-   * components:
-   *   responses:
-   *     UserLoggedIn:
-   *       description: Login was successful
-   *       content:
-   *         application/json:
-   *           schema:
-   *             allOf:
-   *               - $ref: '#/components/schemas/SuccessResponse'
-   *               - type: object
-   *                 properties:
-   *                   message:
-   *                     example: Account login successfully
-   *                   body:
-   *                     type: object
-   *                     properties:
-   *                       token:
-   *                         type: string
-   *                         description: User's login token
-   *                         example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.\
-   *                           eyJlbWFpbCI6Imp1YW4ucGVyZXpAY29tcGFueS5jb20iLCJpYXQiOjE2NTk5Nzg1MDgs\
-   *                           ImV4cCI6MTY1OTk4MjEwOH0.\
-   *                           0vApbsxQ6vqbumair2kFs58JuMX470XPrQ6nDd7BwO4"
-   *     LoginInvalidCredentials:
-   *       description: Email or password are incorrect
-   *       content:
-   *         text/html:
-   *           schema:
-   *             type: string
-   *             example: >-
-   *               UnauthorizedError:
-   *               [Error login account] - [auth/login - POST]: Invalid Credentials
-   *     LoginValidationError:
-   *       description: The request body validation failed
-   *       content:
-   *         application/json:
-   *           schema:
-   *             allOf:
-   *             - $ref: '#/components/schemas/ValidationError'
-   *             - type: object
-   *               properties:
-   *                 errors:
-   *                   example:
-   *                     - value: notAnEmail
-   *                       msg: Must be an email
-   *                       param: email
-   *                       location: body
-   */
-
   login: catchAsync(async (req, res, next) => {
     const {
       body: {
@@ -233,32 +139,6 @@ module.exports = {
       next(httpError)
     }
   }),
-
-  /**
-   * @swagger
-   * components:
-   *   responses:
-   *     UserDataRetrieved:
-   *       description: The user data was retrieved successfully
-   *       content:
-   *         application/json:
-   *           schema:
-   *             allOf:
-   *               - $ref: '#/components/schemas/SuccessResponse'
-   *               - type: object
-   *                 properties:
-   *                   message:
-   *                     example: User retrieved successfully
-   *                   body:
-   *                     $ref: '#/components/schemas/User'
-   *     InvalidToken:
-   *       description: The authorization token is invalid or nonexistent
-   *       content:
-   *         text/html:
-   *           schema:
-   *             type: string
-   *             example: 'UnauthorizedError: Invalid login credentials'
-   */
 
   getData: (req, res) => {
     const { user } = req
--- routes/auth.js
+++ routes/auth.js
@@ -114,6 +114,30 @@
  *         - firstName
  *         - lastName
  *
+ *   responses:
+ *     LoginValidationError:
+ *       description: The request body validation failed
+ *       content:
+ *         application/json:
+ *           schema:
+ *             allOf:
+ *             - $ref: '#/components/schemas/ValidationError'
+ *             - type: object
+ *               properties:
+ *                 errors:
+ *                   example:
+ *                     - value: notAnEmail
+ *                       msg: Must be an email
+ *                       param: email
+ *                       location: body
+ *     InvalidToken:
+ *       description: The authorization token is invalid or nonexistent
+ *       content:
+ *         text/html:
+ *           schema:
+ *             type: string
+ *             example: 'UnauthorizedError: Invalid login credentials'
+ *
  * paths:
  *   /auth/register:
  *     post:
@@ -128,9 +152,44 @@
  *               $ref: '#/components/schemas/User'
  *       responses:
  *         201:
- *           $ref: '#/components/responses/UserRegistered'
+ *           description: The account was registered successfully
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 allOf:
+ *                   - $ref: '#/components/schemas/SuccessResponse'
+ *                   - type: object
+ *                     properties:
+ *                       code:
+ *                         example: 201
+ *                       message:
+ *                         example: Account registered successfully
+ *                       body:
+ *                         type: object
+ *                         properties:
+ *                           user:
+ *                             $ref: '#/components/schemas/User'
+ *                           token:
+ *                             type: string
+ *                             description: User's login token
+ *                             example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.\
+ *                               eyJlbWFpbCI6Imp1YW4ucGVyZXpAY29tc\
+ *                               GFueS5jb20iLCJpYXQiOjE2NTk5Nzg1MDgs\
+ *                               ImV4cCI6MTY1OTk4MjEwOH0.\
+ *                               0vApbsxQ6vqbumair2kFs58JuMX470XPrQ6nDd7BwO4"
  *         400:
- *           $ref: '#/components/responses/RegisterBadRequest'
+ *           allOf:
+ *             - $ref: '#/components/responses/LoginValidationError'
+ *             - description: >-
+ *                 The request body validation failed or the provided email is already in use
+ *               content:
+ *                 text/html:
+ *                   schema:
+ *                     type: string
+ *                     example: >-
+ *                       BadRequestError:
+ *                       [Error registering account] - [auth/register - POST]:
+ *                       Email is already in use
  *
  *   /auth/login:
  *     post:
@@ -145,11 +204,38 @@
  *               $ref: '#/components/schemas/LoginBody'
  *       responses:
  *         200:
- *           $ref: '#/components/responses/UserLoggedIn'
+ *           description: Login was successful
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 allOf:
+ *                   - $ref: '#/components/schemas/SuccessResponse'
+ *                   - type: object
+ *                     properties:
+ *                       message:
+ *                         example: Account login successfully
+ *                       body:
+ *                         type: object
+ *                         properties:
+ *                           token:
+ *                             type: string
+ *                             description: User's login token
+ *                             example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.\
+ *                               eyJlbWFpbCI6Imp1YW4ucGVyZXpAY29tc\
+ *                               GFueS5jb20iLCJpYXQiOjE2NTk5Nzg1MDgs\
+ *                               ImV4cCI6MTY1OTk4MjEwOH0.\
+ *                               0vApbsxQ6vqbumair2kFs58JuMX470XPrQ6nDd7BwO4"
  *         400:
  *           $ref: '#/components/responses/LoginValidationError'
  *         401:
- *           $ref: '#/components/responses/LoginInvalidCredentials'
+ *           description: Email or password are incorrect
+ *           content:
+ *             text/html:
+ *               schema:
+ *                 type: string
+ *                 example: >-
+ *                   UnauthorizedError:
+ *                   [Error login account] - [auth/login - POST]: Invalid Credentials
  *
  *   /auth/me:
  *     get:
@@ -160,7 +246,18 @@
  *         - bearerAuth: []
  *       responses:
  *         200:
- *           $ref: '#/components/responses/UserDataRetrieved'
+ *           description: The user data was retrieved successfully
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 allOf:
+ *                   - $ref: '#/components/schemas/SuccessResponse'
+ *                   - type: object
+ *                     properties:
+ *                       message:
+ *                         example: User retrieved successfully
+ *                       body:
+ *                         $ref: '#/components/schemas/User'
  *         401:
  *           $ref: '#/components/responses/InvalidToken'
  */
