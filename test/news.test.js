721036f6e72a0f107cc836ca6541f5c9bbdbc2a8
ghnoob <ptnchk.rodrigo@gmail.com>
2022-08-15 22:39:42 -0300

    GET /news/:id tests addded

--- test/news.test.js
+++ test/news.test.js
@@ -2,28 +2,94 @@ const { expect } = require('chai');
 const request = require('supertest');
 const app = require('../app')
 
-describe('news tests on success', () => {
-  describe('[GET - /news]', () => {
-    it('Should return paginated array of news', async () => {
-      const response = await request(app).get('/news');
 
-      expect(response.statusCode).to.equal(200);
+describe('news tests', () => {
+  let userToken, adminToken;
 
-      expect(response.body)
-        .to.have.property('body')
-        .that.has.property('urls');
+  before('get login tokens', async () => {
+    const userLoginResponse = await request(app)
+      .post('/auth/login')
+      .send({
+        email: 'usuariocomun1@mail.com',
+        password: 'ABCD_efgh_1234',
+      })
+      .expect(200);
 
-      const { body: { body } } = response;
+    userToken = userLoginResponse.body.body;
 
-      expect(body.urls)
-        .to.have.property('prevUrl')
-        .that.is.a.string;
+    const adminLoginResponse = await request(app)
+      .post('/auth/login')
+      .send({
+        email: 'usuarioadmin1@mail.com',
+        password: 'ABCZ_efgi_1230',
+      })
+      .expect(200);
 
-      expect(body.urls)
-        .to.have.property('nextUrl')
-        .that.is.a.string;
+    adminToken = adminLoginResponse.body.body;
+  });
 
-      expect(Array.isArray(body.news)).to.be.true;
+  describe('news tests on success', () => {
+    describe('[GET - /news]', () => {
+      it('Should return paginated array of news', async () => {
+        const response = await request(app).get('/news');
+
+        expect(response.statusCode).to.equal(200);
+
+        expect(response.body)
+          .to.have.property('body')
+          .that.has.property('urls');
+
+        const { body: { body } } = response;
+
+        expect(body.urls)
+          .to.have.property('prevUrl')
+          .that.is.a.string;
+
+        expect(body.urls)
+          .to.have.property('nextUrl')
+          .that.is.a.string;
+
+        expect(Array.isArray(body.news)).to.be.true;
+      });
+    });
+
+    describe('[GET - /news/:id]', () => {
+      it('should return a single new', async () => {
+        const response = await request(app)
+          .get('/news/1')
+          .set('Authorization', `Bearer ${adminToken}`);
+
+        expect(response.statusCode).to.equal(200);
+        expect(response.body)
+          .to.have.property('body')
+          .that.has.property('id');
+      });
+    });
+  });
+
+  describe('news tests on failure', () => {
+    describe('[GET - /news/:id]', () => {
+      it('should return 401 if no token provided', async () => {
+        const response = await request(app).get('/news/1');
+
+        expect(response.statusCode).to.equal(401);
+      });
+
+      it('should return 403 if user is not an admin', async () => {
+        const response = await request(app)
+          .get('/news/1')
+          .set('Authorization', `Bearer ${userToken}`);
+
+        expect(response.statusCode).to.equal(403);
+      });
+
+      it('should return 404 if new is not found', async () => {
+        const response = await request(app)
+          .get('/news/notanid')
+          .set('Authorization', `Bearer ${adminToken}`);
+
+        expect(response.statusCode).to.equal(404);
+      });
     });
   });
 });
