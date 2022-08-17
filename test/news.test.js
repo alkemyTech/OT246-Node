b7632c796df00ea9a3ec9df6951c38d2e4c64400
Rodrigo Pietnechuk <ptnchk.rodrigo@gmail.com>
2022-08-17 01:20:42 -0300

    [TEST] Test: Endpoints /news OT246-298 (#103)

---
+++ test/news.test.js
@@ -0,0 +1,326 @@
+const { expect } = require('chai');
+const request = require('supertest');
+const app = require('../app');
+const { New } = require('../database/models');
+
+
+describe('news tests', () => {
+  let userToken, adminToken;
+
+  before('get login tokens', async () => {
+    const userLoginResponse = await request(app)
+      .post('/auth/login')
+      .send({
+        email: 'usuariocomun1@mail.com',
+        password: 'ABCD_efgh_1234',
+      })
+      .expect(200);
+
+    userToken = userLoginResponse.body.body;
+
+    const adminLoginResponse = await request(app)
+      .post('/auth/login')
+      .send({
+        email: 'usuarioadmin1@mail.com',
+        password: 'ABCZ_efgi_1230',
+      })
+      .expect(200);
+
+    adminToken = adminLoginResponse.body.body;
+  });
+
+  describe('news tests on success', () => {
+    describe('[GET - /news]', () => {
+      it('Should return paginated array of news', async () => {
+        const response = await request(app).get('/news');
+
+        expect(response.statusCode).to.equal(200);
+
+        expect(response.body)
+          .to.have.property('body')
+          .that.has.property('urls');
+
+        const { body: { body } } = response;
+
+        expect(body.urls)
+          .to.have.property('prevUrl')
+          .that.is.a.string;
+
+        expect(body.urls)
+          .to.have.property('nextUrl')
+          .that.is.a.string;
+
+        expect(Array.isArray(body.news)).to.be.true;
+      });
+    });
+
+    describe('[GET - /news/:id]', () => {
+      it('should return a single new', async () => {
+        const response = await request(app)
+          .get('/news/1')
+          .set('Authorization', `Bearer ${adminToken}`);
+
+        expect(response.statusCode).to.equal(200);
+        expect(response.body)
+          .to.have.property('body')
+          .that.has.property('id');
+      });
+    });
+
+    describe('[GET - /news/:id/comments', () => {
+      it('should return an array of comments', async () => {
+        const response = await request(app)
+          .get('/news/1/comments')
+          .set('Authorization', `Bearer ${userToken}`);
+
+        expect(response.statusCode).to.equal(200);
+
+        expect(response.body).to.have.property('body');
+        expect(Array.isArray(response.body.body)).to.be.true;
+      });
+    });
+
+    describe('[POST - /news', () => {
+      it('should create a new', async () => {
+        const response = await request(app)
+          .post('/news')
+          .set('Authorization', `Bearer ${adminToken}`)
+          .send({
+            name: 'title',
+            content: 'body',
+            image: 'https://foo.bar',
+            categoryId: 1,
+          });
+
+        expect(response.statusCode).to.equal(201);
+        expect(response.body)
+          .to.have.property('body')
+          .that.has.property('id');
+      });
+    });
+
+    describe('[PUT - /news/:id', () => {
+      it('should update a new', async () => {
+        const response = await request(app)
+          .put('/news/1')
+          .set('Authorization', `Bearer ${adminToken}`)
+          .send({
+            name: 'edited',
+          });
+
+        expect(response.statusCode).to.equal(200);
+        expect(response.body)
+          .to.have.property('body')
+          .that.has.property('name', 'edited');
+      });
+    });
+
+    describe('[DELETE - /news/:id]', () => {
+      it('should delete a new', async () => {
+        const response = await request(app)
+          .delete('/news/1')
+          .set('Authorization', `Bearer ${adminToken}`);
+
+        expect(response.statusCode).to.equal(200);
+      });
+    });
+
+    after('Restore the deleted new so its easier to run the test again', async () => {
+      await New.restore({ where: { id: 1 }, limit: 1 });
+    });
+  });
+
+  describe('news tests on failure', () => {
+    describe('[GET - /news/:id]', () => {
+      it('should return 401 if no token provided', async () => {
+        const response = await request(app).get('/news/1');
+
+        expect(response.statusCode).to.equal(401);
+      });
+
+      it('should return 403 if user is not an admin', async () => {
+        const response = await request(app)
+          .get('/news/1')
+          .set('Authorization', `Bearer ${userToken}`);
+
+        expect(response.statusCode).to.equal(403);
+      });
+
+      it('should return 404 if new is not found', async () => {
+        const response = await request(app)
+          .get('/news/notanid')
+          .set('Authorization', `Bearer ${adminToken}`);
+
+        expect(response.statusCode).to.equal(404);
+      });
+    });
+
+    describe('[GET - /news/:id/comments]', () => {
+      it('should return 401 if no token provided', async () => {
+        const response = await request(app).get('/news/1/comments');
+
+        expect(response.statusCode).to.equal(401);
+      });
+
+      it('should return 404 if new is not found', async () => {
+        const response = await request(app)
+          .get('/news/notanid/comments')
+          .set('Authorization', `Bearer ${userToken}`);
+
+        expect(response.statusCode).to.equal(404);
+      });
+    });
+
+    describe('[POST - /news', () => {
+      it('should return 400 if request body is not valid', async () => {
+        const response = await request(app)
+          .post('/news')
+          .set('Authorization', `Bearer ${adminToken}`)
+          .send({
+            name: 'title',
+            content: 'body',
+            image: 'https://foo.bar',
+            categoryId: 'not a number',
+          });
+
+        expect(response.statusCode).to.equal(400);
+      });
+
+      it('should return 401 if no token provided', async () => {
+        const response = await request(app)
+          .post('/news')
+          .send({
+            name: 'title',
+            content: 'body',
+            image: 'https://foo.bar',
+            categoryId: 1,
+          });
+
+        expect(response.statusCode).to.equal(401);
+      });
+
+      it('should return 403 if user is not admin', async () => {
+        const response = await request(app)
+          .post('/news')
+          .set('Authorization', `Bearer ${userToken}`)
+          .send({
+            name: 'title',
+            content: 'body',
+            image: 'https://foo.bar',
+            categoryId: 1,
+          });
+
+        expect(response.statusCode).to.equal(403);
+      });
+
+      it('should return 404 if category is not found', async () => {
+        const response = await request(app)
+          .post('/news')
+          .set('Authorization', `Bearer ${adminToken}`)
+          .send({
+            name: 'title',
+            content: 'body',
+            image: 'https://foo.bar',
+            categoryId: -1,
+          });
+
+        expect(response.statusCode).to.equal(404);
+      });
+    });
+  });
+
+  describe('[PUT - /news/:id', () => {
+    it('should return 400 if request body is not valid', async () => {
+      const response = await request(app)
+        .put('/news/1')
+        .set('Authorization', `Bearer ${adminToken}`)
+        .send({
+          name: 'title',
+          content: 'body',
+          image: 'https://foo.bar',
+          categoryId: 'not a number',
+        });
+
+      expect(response.statusCode).to.equal(400);
+    });
+
+    it('should return 401 if no token provided', async () => {
+      const response = await request(app)
+        .put('/news/1')
+        .send({
+          name: 'title',
+          content: 'body',
+          image: 'https://foo.bar',
+          categoryId: 1,
+        });
+
+      expect(response.statusCode).to.equal(401);
+    });
+
+    it('should return 403 if user is not admin', async () => {
+      const response = await request(app)
+        .put('/news/1')
+        .set('Authorization', `Bearer ${userToken}`)
+        .send({
+          name: 'title',
+          content: 'body',
+          image: 'https://foo.bar',
+          categoryId: 1,
+        });
+
+      expect(response.statusCode).to.equal(403);
+    });
+
+    it('should return 404 if new is not found', async () => {
+      const response = await request(app)
+        .put('/news/notanid')
+        .set('Authorization', `Bearer ${adminToken}`)
+        .send({
+          name: 'title',
+          content: 'body',
+          image: 'https://foo.bar',
+          categoryId: 1,
+        });
+
+      expect(response.statusCode).to.equal(404);
+    });
+
+    it('should return 404 if category is not found', async () => {
+      const response = await request(app)
+        .put('/news/1')
+        .set('Authorization', `Bearer ${adminToken}`)
+        .send({
+          name: 'title',
+          content: 'body',
+          image: 'https://foo.bar',
+          categoryId: -1,
+        });
+
+      expect(response.statusCode).to.equal(404);
+    });
+  });
+
+  describe('[DELETE - /news/:id]', () => {
+    it('should return 401 if no token provided', async () => {
+      const response = await request(app).delete('/news/1');
+
+      expect(response.statusCode).to.equal(401);
+    });
+
+    it('should return 403 if user is not an admin', async () => {
+      const response = await request(app)
+        .delete('/news/1')
+        .set('Authorization', `Bearer ${userToken}`);
+
+      expect(response.statusCode).to.equal(403);
+    });
+
+    it('should return 404 if new is not found', async () => {
+      const response = await request(app)
+        .delete('/news/notanid')
+        .set('Authorization', `Bearer ${adminToken}`);
+
+      expect(response.statusCode).to.equal(404);
+    });
+  });
+});
