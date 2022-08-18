f59ae35ff4cc5a7dd2c6172f50a78e7fcfb51220
Martin Heredia <martinheredia.dev@gmail.com>
2022-08-18 16:51:36 -0300

    fixed requeriments in validation schema

--- schemas/organization.js
+++ schemas/organization.js
@@ -2,11 +2,6 @@ const organizationBody = {
   name: {
     in: 'body',
     trim: true,
-    isLength: { options: { min: 4 }, errorMessage: 'Name must be at least 4 characters long' },
-    matches: {
-      options: [/^[a-zA-Z ]*$/],
-      errorMessage: 'Name must be alphabetic',
-    },
     notEmpty: { errorMessage: 'Must not be empty' },
     optional: true,
   },
@@ -21,11 +16,6 @@ const organizationBody = {
     in: 'body',
     trim: true,
     notEmpty: { errorMessage: 'Must not be empty' },
-    matches: {
-      options: [/^[A-Za-z]+ ?[0-9]+$/],
-      errorMessage: 'Must be a valid address',
-    },
-    isLength: { options: { min: 5 }, errorMessage: 'Address must be at least 5 characters long' },
     optional: true,
   },
   phone: {
