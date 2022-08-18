7c409b630d33650407b02cf22a8f98cc25605ccf
Martin Heredia <martinheredia.dev@gmail.com>
2022-08-18 20:43:50 -0300

    fixed error in middleware ownership, and added to endpoint of users

--- middlewares/ownership.js
+++ middlewares/ownership.js
@@ -1,16 +1,20 @@
+const createHttpError = require('http-errors')
 const { authUser } = require('./authUser')
 
 module.exports = {
   ownership: [
     authUser,
     (req, res, next) => {
-      const userId = req.params?.id
-      const { user } = req.user
-
+      const userId = Number(req.params.id)
+      const { user } = req
       if (userId === user.id || user.roleId === 1) {
         next()
       } else {
-        res.status(403).send()
+        const httpError = createHttpError(
+          401,
+          'Invalid login credentials',
+        )
+        next(httpError)
       }
     },
   ],
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const { get, destroy, put } = require('../controllers/users')
 const { authUserAdmin } = require('../middlewares/authUserAdmin')
-const { authUser } = require('../middlewares/authUser')
+const { ownership } = require('../middlewares/ownership')
 
 const router = express.Router()
 
 router.get('/', authUserAdmin, get)
-router.delete('/:id', authUser, destroy)
-router.put('/:id', authUser, put)
+router.delete('/:id', ownership, destroy)
+router.put('/:id', ownership, put)
 
 module.exports = router
