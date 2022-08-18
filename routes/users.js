068bebc53acf9a6a25d42060da675670969f9498
Martin Heredia <martinheredia.dev@gmail.com>
2022-08-18 16:35:47 -0300

    added middlewares in user endpoints

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const { get, destroy, put } = require('../controllers/users')
 const { authUserAdmin } = require('../middlewares/authUserAdmin')
+const { authUser } = require('../middlewares/authUser')
 
 const router = express.Router()
 
 router.get('/', authUserAdmin, get)
-router.delete('/:id', destroy)
-router.put('/:id', put)
+router.delete('/:id', authUser, destroy)
+router.put('/:id', authUser, put)
 
 module.exports = router
