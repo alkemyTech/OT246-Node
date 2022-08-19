96c8b11af173bc671b8e8b47ceffa3d32232d253
Martin Heredia <85527959+kamp3r@users.noreply.github.com>
2022-08-18 22:14:27 -0300

    [FIX] Added middleware AuthUser to endpoint user (#110)
    * added middlewares in user endpoints
    
    * fixed error in middleware ownership, and added to endpoint of users

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const { get, destroy, put } = require('../controllers/users')
 const { authUserAdmin } = require('../middlewares/authUserAdmin')
+const { ownership } = require('../middlewares/ownership')
 
 const router = express.Router()
 
 router.get('/', authUserAdmin, get)
-router.delete('/:id', destroy)
-router.put('/:id', put)
+router.delete('/:id', ownership, destroy)
+router.put('/:id', ownership, put)
 
 module.exports = router
